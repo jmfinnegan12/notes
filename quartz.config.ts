518e9d39978861ecce16461284cd9ab667c4f076
Jim Finnegan <62609346+jmfinnegan12@users.noreply.github.com>
2026-01-03 21:11:02 -0500

    Change pageTitle from 'Jimbo' to 'Algo Jimbo'

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Jimbo",
+    pageTitle: "Algo Jimbo",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
