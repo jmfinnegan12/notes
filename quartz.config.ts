474dcfa6782b30fe3413ab4763ce2b3f8841722f
Jim Finnegan <62609346+jmfinnegan12@users.noreply.github.com>
2026-04-08 09:40:16 -0700

    Update pageTitle to 'Jimbo Homepage'

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Jimbo",
+    pageTitle: "Jimbo Homepage",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
